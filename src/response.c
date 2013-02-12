72d305e19e0bd07c5d136024b06e53ca12bc4218
Patrick Reagan <reaganpr@gmail.com>
2013-02-12 08:56:24 -0700

    Serve up default document if available when requesting a path

--- src/response.c
+++ src/response.c
@@ -1,36 +1,15 @@
 #include <string.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 #include <buffer.h>
 
 #include <response.h>
 #include <header.h>
+#include <file.h>
 
 void response_add_status(Response *response, int status_code);
 void response_add_header_int(Response *response, const char *key, int value);
 void response_add_header_str(Response *response, const char *key, char *value);
-
-int
-is_file(char *filename)
-{
-    struct stat fs;
-
-    if (stat(filename, &fs) == 0) {
-        return (fs.st_mode & S_IFREG) == S_IFREG;
-    }
-
-    return 0;
-}
-
-FILE *
-file_open_from_path(char *filename) {
-    if (is_file(filename)) {
-        return fopen(filename, "r");
-    }
-
-    return NULL;
-}
 
 Response *
 response_create(char *filename)
